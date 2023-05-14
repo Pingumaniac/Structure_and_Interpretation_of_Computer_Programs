6582286ea0b9e5096b1838a2aec194df6f0a9d92
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-05-14 17:11:09 -0500

    Delete Chapter 1 directory

--- Chapter 1/ex1_2.scm
+++
@@ -1,3 +0,0 @@
-#lang scheme
-(/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
-   (* 3 (- 6 2) (- 2 7))) ; written in prefix form and the answer is -37/150
--- Chapter 1/ex1_4.scm
+++
@@ -1,7 +0,0 @@
-#lang scheme
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-#|
-This function returns the value of a + b if b is higher than 0,
-but returns a - b if b is lower than 0.
-|#
